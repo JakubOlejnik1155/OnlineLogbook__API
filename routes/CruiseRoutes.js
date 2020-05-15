183c176a146cd1e065c4fa4e85a8614a6890844e
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-05-16 00:41:54 +0200

    change adding boat to cruise

--- routes/CruiseRoutes.js
+++ routes/CruiseRoutes.js
@@ -33,7 +33,7 @@ router.post("", authenticateToken, async (req, res) => {
         newBoatID = isBoatExists._id;
     }
     //add cruise
-    const cruiseObject = new Cruise({ ...cruise, boatID: newBoatID, userID: userID });
+    const cruiseObject = new Cruise({ ...cruise, boatID: boat, userID: userID });
     const currentCruiseObject = new CurrentCruise({userID: userID, cruiseID: cruiseObject._id});
 
     const isCurrentObjectForThisUser = await CurrentCruise.findOne({userID: userID});
@@ -55,24 +55,79 @@ router.post("", authenticateToken, async (req, res) => {
 router.get("", authenticateToken, async (req, res) => {
     const userID = req.id.userId;
 
+
+    let cruises = [];
+
     let cruiseArray;
-    await Cruise.find({userID: userID}, async (err, data) => {
+    await Cruise.find({ userID: userID }, (err, data) => {
         if (err) return res.status(500).send({ error: { code: 500, msg: "Internal Server Error" } });
-        else{
-            cruiseArray = data;
-            for (let i = 0; i < cruiseArray.length; i++) {
-                await Boat.findOne({ _id: cruiseArray[i].boatID }, async (err, boat) => {
-                    if (boat) {
-                        // cruiseArray[i] = { cruise: cruiseArray[i], boat: boat }
-                        await Day.find({cruiseID: cruiseArray[i]._id},(err, day)=>{
-                            if(day) cruiseArray[i] = { cruise: cruiseArray[i], boat: boat , days: day}
-                        })
-                    } else if (err) return res.status(500).send({ error: { code: 500, msg: "Internal Server Error1" } });
-                })
-            }
-            return res.status(200).send({ data: cruiseArray })
-        }
+        else if (data) cruiseArray = data;
     });
+
+    let boat;
+    await Boat.find({ userID: userID}, (err, data) => {
+        if (err) return res.status(500).send({ error: { code: 500, msg: "Internal Server Error" } });
+        else if (data) boat = data;
+    });
+
+    let days;
+    await Day.find({userID: userID}, (err, data) => {
+        if (err) return res.status(500).send({ error: { code: 500, msg: "Internal Server Error" } });
+        else if (data) days = data;
+    });
+
+
+
+
+
+
+    async function asyncForEach(array, callback) {
+        for (let index = 0; index < array.length; index++) {
+            await callback(array[index], index, array);
+        }
+    }
+if(cruiseArray && boat && days){
+    await asyncForEach(cruiseArray, async (cruise) => {
+        console.log(cruise)
+    await asyncForEach(boat, async (b)=>{
+        console.log(b)
+             if(b._id.toString() === cruise.boatID)
+                cruises.push({cruise: cruise, boat: b});
+        })
+    })
+}
+    console.log('Done');
+
+    // await cruiseArray.forEach(async (cruise, index) => {
+    // await boat.forEach( boat => {
+    //         if(boat._id === cruise.boatID)
+    //             cruiseArray[index] = {cruise: cruise, boat: boat};
+    //     })
+    // });
+
+    return res.status(200).send({ cruises: cruises})
+
+
+
+
+    // let cruiseArray;
+    // await Cruise.find({userID: userID}, (err, data) => {
+    //     if (err) return res.status(500).send({ error: { code: 500, msg: "Internal Server Error" } });
+    //     else{
+    //         cruiseArray = data;
+    //         for (let i = 0; i < cruiseArray.length; i++) {
+    //             Boat.findOne({ _id: cruiseArray[i].boatID }, async (err, boat) => {
+    //                 if (boat) {
+    //                     //cruiseArray[i] = { cruise: cruiseArray[i], boat: boat }
+    //                     Day.find({cruiseID: cruiseArray[i]._id},(err, day)=>{
+    //                         if(day) cruiseArray[i] = { cruise: cruiseArray[i], boat: boat , days: day}
+    //                     })
+    //                 } else if (err) return res.status(500).send({ error: { code: 500, msg: "Internal Server Error1" } });
+    //                 return res.status(200).send({ data: cruiseArray })
+    //             })
+    //         }
+    //     }
+    // });
 });
 
 // GET /api/cruises/current => check if is active cruise for user
