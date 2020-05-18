8e93b6649b479653a6686edcbdaef8c0b7982f33
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-05-18 22:55:44 +0200

    change pdf font to Roboto

--- documents/index.js
+++ documents/index.js
@@ -51,6 +51,7 @@ module.exports = (data) =>{
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap" rel="stylesheet">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -59,6 +60,7 @@ module.exports = (data) =>{
 
         <style>
         * {
+            font-family: Roboto, arial;
             font-size: 10px;
             margin: 0;
             padding: 0;
