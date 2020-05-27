2b890999d3ada36393ad7688273815baf2668296
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-05-27 23:48:45 +0200

    move authRoutes to routes.js file

--- routes/routes.js
+++ routes/routes.js
@@ -11,6 +11,7 @@ const {
   registerValidation,
   setNewPasswordValidation
 } = require("../function/validation");
+const { loginValidation } = require("../function/validation");
 
 //registration breakpoint
 router.post("/register", async (req, res) => {
@@ -183,6 +184,68 @@ router.get("", authenticateToken, async (req, res)=>{
 router.get('/try', authenticateToken, (req, res)=>{
   return res.send({message: "Authorized", userID: res.id});
 });
+
+
+router.post("/login", async (req, res) => {
+  //validate data before logging in
+  const { error } = loginValidation(req.body);
+  if (error) return res.send({ error: error.details[0].message });
+  //if account exists
+  const user = await User.findOne({
+    email: req.body.email
+  });
+  if (!user) return res.send({ error: "Email not found" });
+  //is password correct
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.send({ error: "password is wrong" });
+  //is user authorized
+  if (!user.isAuthorized) return res.send({ error: "please verify your email before logging in" });
+  //generating tokens
+  const userId = user._id;
+  const userObject = { userId: userId };
+  try {
+    const accessToken = await generateAccessToken(userObject);
+    const refreshToken = await jwt.sign(userObject, process.env.REFRESH_TOKEN_SECRET);
+    //adding logged user to DB
+    const DBtoken = new RJwt({ RJwt: refreshToken });
+    await DBtoken.save();
+    res.send({ accessToken: accessToken, refreshToken: refreshToken });
+  } catch (e) { return res.send({ error: "something went wrong" }); }
+});
+
+router.delete("/logout", async (req, res) => {
+  const refreshTokenToDelete = req.body.RJwt;
+  try {
+    await RJwt.deleteOne({ RJwt: refreshTokenToDelete });
+    res.sendStatus(204);
+  } catch (e) {
+    res.send(e);
+  }
+});
+//ask for new token
+router.post("/token", async (req, res) => {
+  const refreshToken = req.body.RJwt;
+  if (!refreshToken) return res.send({ status: 401, error: true, message: "something went wrong" });
+  const DBtoken = RJwt.findOne({ RJwt: refreshToken });
+  if (!DBtoken) return res.send({ status: 403, error: true, message: "something went wrong" });
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (error, decoded) => {
+    if (error) return res.send({ status: 403, error: true, message: "something went wrong" });
+    const newAccessToken = generateAccessToken(decoded.userId);
+    res.send({ status: 200, error: false, message: "new token sent", JWT: newAccessToken });
+  });
+});
+
+
+//@TODO delete before production
+router.post("/jwt/decode", async (req, res) => {
+  const user = jwt.decode(req.body.jwt);
+  const ex = jwt.verify(req.body.jwt, process.env.TOKEN_SECRET,
+    (error, decoded) => {
+      if (error) res.send(error);
+      res.send(decoded);
+    });
+});
+
 
 function authenticateToken(req, res, next){
   const authHeader = req.headers['authorization'];
